14559760438f90551a7150b65e93942c011d1c0f
Sergey Smirnov <sergous@gmail.com>
2017-08-06 22:49:46 +0300

    save note to db.

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,6 +1,13 @@
 module.exports = function(app, db) {
     app.post('/notes', (req, res) => {
-        console.log(req.body);
-        res.send('Hello')
+        const note = { text: req.body.body, title: req.body.title };
+        db.collection('notes').insert(note, (err, result) => {
+            if (err) {
+                res.send({ 'error': 'An error has occured' });
+                return;
+            }
+
+            res.send(result.ops[0]);
+        });
     });
 };
