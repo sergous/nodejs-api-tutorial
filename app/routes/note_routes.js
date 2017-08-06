d5ba65c2de057e3bf3ac52daef8f0b21eb85cf70
Sergey Smirnov <sergous@gmail.com>
2017-08-06 23:08:48 +0300

    add update note route.

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -26,6 +26,20 @@ module.exports = function(app, db) {
         });
     });
 
+    app.put('/notes/:id', (req, res) => {
+        const id = req.params.id;
+        const details = { '_id': new ObjectID(id) };
+        const note = { text: req.body.body, title: req.body.title };
+        db.collection('notes').update(details, note, (err, item) => {
+            if (err) {
+                res.send({ 'error': 'An error has occured'} );
+                return;
+            }
+
+            res.send(note);
+        });
+    });
+
     app.delete('/notes/:id', (req, res) => {
         const id = req.params.id;
         const details = { '_id': new ObjectID(id) };
