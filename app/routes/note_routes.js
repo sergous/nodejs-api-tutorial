3aa258bf8e670c52d1fb730e8ca92267f2ed7868
Sergey Smirnov <sergous@gmail.com>
2017-08-06 22:58:11 +0300

    get note by id.

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,4 +1,19 @@
+const ObjectID = require('mongodb').ObjectID;
+
 module.exports = function(app, db) {
+    app.get('/notes/:id', (req, res) => {
+        const id = req.params.id;
+        const details = { '_id': new ObjectID(id) };
+        db.collection('notes').findOne(details, (err, item) => {
+            if (err) {
+                res.send( { 'error': 'An error has occured'} );
+                return;
+            }
+
+            res.send(item);
+        })
+    });
+
     app.post('/notes', (req, res) => {
         const note = { text: req.body.body, title: req.body.title };
         db.collection('notes').insert(note, (err, result) => {
